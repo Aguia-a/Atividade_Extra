372aea27317632517539518be84a6d2b2a31ce36
Kauan <aguiagames4@gmail.com>
2024-11-28 20:46:07 -0300

    selection_sort.c - Adicionados os Comentários Necessários

--- selection_sort.c
+++ selection_sort.c
@@ -1,29 +1,43 @@
 #include <stdio.h>
 
+/*
+
+    Algoritmo de Selection Sort em C
+
+    Realiza a ordenacao de um vetor de 5 elementos na ordem crescente
+
+*/
+
 int main()
 {
 
-    int vetor[] = {3, 2, 5, 1, 4};
-    int temp;
-    int x, y;
+    /*-Declaracao do Vetor e Variaveis-*/
+    int vetor[] = {3, 2, 5, 1, 4};     // Vetor com valores desordenados
+    int temp;                          // Variavel de armazenamento temporario
+    int x, y;                          // Variaveis para controle dos loops
+    /*---------------------------------*/
 
+    /*-Imprime o Vetor Antes da Ordenacao-*/
     printf("Vetor Nao Ordenado: \t");
-
     for(x = 0; x < 5; x++)
     {
 
         printf("%d ", vetor[x]);
 
     }
+    /*------------------------------------*/
 
     printf("\n");
 
+    /*-Loop Responsavel pela Ordenacao do Vetor-*/
+    // Loop Principal (Define a posicao atual que sera preenchida)
     for(x = 0; x < 5; x++)
     {
-
+        // Loop Interno, responsavel por encontrar o menor valor entre aqueles
+        // que nao foram ordenados
         for(y = x + 1; y < 5; y++)
         {
-
+            // Se encontrado um valor inferior ao atual, faz a troca
             if(vetor[x] > vetor[y])
             {
 
@@ -36,15 +50,17 @@ int main()
         }
 
     }
+    /*------------------------------------------*/
 
+    /*-Imprime o Vetor Apos a Ordenacao-*/
     printf("Vetor Ordenado: \t");
-
     for(x = 0; x < 5; x++)
     {
 
         printf("%d ", vetor[x]);
 
     }
+    /*----------------------------------*/
     
     return 0;
 
