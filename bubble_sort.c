dfb95d830eff527a895dc0cc22b1307a84b8016b
Kauan <aguiagames4@gmail.com>
2024-11-27 23:22:12 -0300

    Adicionados Comentários ao código bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 
+/*
+
+    Algoritmo de Bubble Sort em C
+
+    Realiza a ordenacao de um vetor de 5 elementos na ordem crescente
+
+*/
+
 int main()
 {
 
-    int vetor[] = {3, 2, 5, 1, 4};
-    int temp;
-    int x, y;
+    /*-Declaracao do Vetor e Variaveis-*/
+    int vetor[] = {3, 2, 5, 1, 4};     // Vetor com valores desordenados
+    int temp;                          // Variavel de armazenamento temporario
+    int x, y;                          // Variavel para controle dos loops
+    /*---------------------------------*/
 
+    /*-Imprime o Vetor Antes da Ordenacao-*/
     printf("Vetor Nao Ordenado: \t");
-
     for(x = 0; x < 5; x++)
     {
 
         printf("%d ", vetor[x]);
 
     }
+    /*------------------------------------*/
     
     printf("\n");
 
+    /*-Loop Resposavel pela Ordenacao do Vetor-*/
+    // Loop que Controla as Repeticoes (Feito de acordo com o tamanho do vetor)
     for(x = 0; x < 5; x++)
     {
-
+        // Loop Interno Responsavel pelas Comparacoes e Trocas
+        // A Cada Repeticao, o maior valor e promovido para o proximo indice
         for(y = 0; y < 5 - x - 1; y++)
         {
-
+            // Compara o Valor Atual com o Valor no Proximo Indice
             if(vetor[y] > vetor[y + 1])
             {
-
+                // Se o valor atual for maior que o proximo, realiza a troca
                 temp = vetor[y];
                 vetor[y] = vetor[y + 1];
                 vetor[y + 1] = temp;
@@ -36,15 +50,17 @@ int main()
         }
 
     }
+    /*-----------------------------------------*/
 
+    /*-Imprime o Vetor Apos a Ordenacao-*/
     printf("Vetor Ordenado: \t");
-
     for(x = 0; x < 5; x++)
     {
 
         printf("%d ", vetor[x]);
 
     }
+    /*----------------------------------*/
 
     return 0;
 
